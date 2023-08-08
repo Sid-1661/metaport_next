fe9169a47dc1391b15963d46e18f89b66e96273c
Talent0407 <imasnpsorrels5@gmail.com>
2023-08-08 20:19:36 +0800

    animation final

--- src/app/expertise/page.jsx
+++ src/app/expertise/page.jsx
@@ -14,6 +14,12 @@ function Expertise() {
   const { t } = useTranslation();
   const [isMouseOver, setIsMouseOver] = useState(false);
   const [isMouseOver1, setIsMouseOver1] = useState(false);
+  const [isMouseOver2, setIsMouseOver2] = useState(false);
+  const [isMouseOver3, setIsMouseOver3] = useState(false);
+  const [isMouseOver4, setIsMouseOver4] = useState(false);
+  const [isMouseOver5, setIsMouseOver5] = useState(false);
+  const [isMouseOver6, setIsMouseOver6] = useState(false);
+  const [isMouseOver7, setIsMouseOver7] = useState(false);
 
 
   const handleMouseEnter = () => {
@@ -30,6 +36,54 @@ function Expertise() {
 
   const handleMouseLeave1 = () => {
     setIsMouseOver1(false);
+  };
+
+  const handleMouseEnter2 = () => {
+    setIsMouseOver2(true);
+  };
+
+  const handleMouseLeave2 = () => {
+    setIsMouseOver2(false);
+  };
+
+  const handleMouseEnter3 = () => {
+    setIsMouseOver3(true);
+  };
+
+  const handleMouseLeave3 = () => {
+    setIsMouseOver3(false);
+  };
+
+  const handleMouseEnter4 = () => {
+    setIsMouseOver4(true);
+  };
+
+  const handleMouseLeave4 = () => {
+    setIsMouseOver4(false);
+  };
+
+  const handleMouseEnter5 = () => {
+    setIsMouseOver5(true);
+  };
+
+  const handleMouseLeave5 = () => {
+    setIsMouseOver5(false);
+  };
+
+  const handleMouseEnter6 = () => {
+    setIsMouseOver6(true);
+  };
+
+  const handleMouseLeave6 = () => {
+    setIsMouseOver6(false);
+  };
+
+  const handleMouseEnter7 = () => {
+    setIsMouseOver7(true);
+  };
+
+  const handleMouseLeave7 = () => {
+    setIsMouseOver7(false);
   };
   return (
     <>
@@ -54,13 +108,38 @@ function Expertise() {
 
       <Row className={Styles.Service}>
         <Col md={6} className={Styles.BlockBack}>
-          <h4>{t("Blockchain & Web3 Development")}</h4>
-          <Image src="/images/block 1.svg" width={218} height={218} className={Styles.Block1} alt='Block1' />
+          <div onMouseEnter={handleMouseEnter2} onMouseLeave={handleMouseLeave2}>
+            {!isMouseOver2 ? (
+              <>
+                <h4>{t("Blockchain & Web3 Development")}</h4>
+                <Image src="/images/block 1.svg" width={218} height={218} className={Styles.Block1} alt='Block1' />
+              </>
+            ) : (
+              <>
+                <h4>{t("Blockchain & Web3 Development")}</h4>
+                <p>{t("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}</p>
+                <Link href={'/contact'} className='c-button fillBtn'>{t("Inquire Now")}</Link>
+              </>
+            )}
+          </div>
         </Col>
         <Col md={6} className={Styles.AiBack}>
-          <h4>{t("AI & ML App ")}</h4>
-          <h4>{t("Development")}</h4>
-          <Image src="/images/innovation 1.svg" width={218} height={218} className={Styles.Innovation1} alt='Innovation1' />
+          <div onMouseEnter={handleMouseEnter3} onMouseLeave={handleMouseLeave3}>
+            {!isMouseOver3 ? (
+              <>
+                <h4>{t("AI & ML App ")}</h4>
+                <h4>{t("Development")}</h4>
+                <Image src="/images/innovation 1.svg" width={218} height={218} className={Styles.Innovation1} alt='Innovation1' />
+              </>
+            ) : (
+              <>
+                <h4>{t("AI & ML App ")}</h4>
+                <h4>{t("Development")}</h4>
+                <p>{t("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}</p>
+                <Link href={'/contact'} className='c-button fillBtn Green'>{t("Inquire Now")}</Link>
+              </>
+            )}
+          </div>
         </Col>
       </Row>
 
@@ -112,7 +191,6 @@ function Expertise() {
                 <Link href={'/contact'} className='c-button fillBtn'>{t("Inquire Now")}</Link>
               </>
             )}
-
           </div>
         </Col>
       </Row>
@@ -136,12 +214,36 @@ function Expertise() {
 
       <Row className={Styles.Service}>
         <Col md={6} className={Styles.BlockBack1}>
-          <h4>{t("AR & VR Develop")}</h4>
-          <Image src="/images/arandvr.svg" width={218} height={218} className={Styles.Block1} alt='Block1' />
+          <div onMouseEnter={handleMouseEnter4} onMouseLeave={handleMouseLeave4}>
+            {!isMouseOver4 ? (
+              <>
+                <h4>{t("AR & VR Develop")}</h4>
+                <Image src="/images/arandvr.svg" width={218} height={218} className={Styles.Block1} alt='Block1' />
+              </>
+            ) : (
+              <>
+                <h4>{t("AR & VR Develop")}</h4>
+                <p>{t("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}</p>
+                <Link href={'/contact'} className='c-button fillBtn'>{t("Inquire Now")}</Link>
+              </>
+            )}
+          </div>
         </Col>
         <Col md={6} className={Styles.AiBack}>
-          <h4>{t("IoT Solutions")}</h4>
-          <Image src="/images/iot.svg" width={218} height={218} className={Styles.Innovation1} alt='Innovation1' />
+          <div onMouseEnter={handleMouseEnter5} onMouseLeave={handleMouseLeave5}>
+            {!isMouseOver5 ? (
+              <>
+                <h4>{t("IoT Solutions")}</h4>
+                <Image src="/images/iot.svg" width={218} height={218} className={Styles.Innovation1} alt='Innovation1' />
+              </>
+            ) : (
+              <>
+                <h4>{t("IoT Solutions")}</h4>
+                <p>{t("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}</p>
+                <Link href={'/contact'} className='c-button fillBtn Black'>{t("Inquire Now")}</Link>
+              </>
+            )}
+          </div>
         </Col>
       </Row>
 
@@ -164,12 +266,36 @@ function Expertise() {
 
       <Row className={Styles.Service}>
         <Col md={6} className={Styles.Design}>
-          <h4>{t("UI/UX Design")}</h4>
-          <Image src="/images/design.svg" width={218} height={218} className={Styles.Block1} alt='Block1' />
+          <div onMouseEnter={handleMouseEnter6} onMouseLeave={handleMouseLeave6}>
+            {!isMouseOver6 ? (
+              <>
+                <h4>{t("UI/UX Design")}</h4>
+                <Image src="/images/design.svg" width={218} height={218} className={Styles.Block1} alt='Block1' />
+              </>
+            ) : (
+              <>
+                <h4>{t("UI/UX Design")}</h4>
+                <p>{t("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}</p>
+                <Link href={'/contact'} className='c-button fillBtn'>{t("Inquire Now")}</Link>
+              </>
+            )}
+          </div>
         </Col>
         <Col md={6} className={Styles.Assurance}>
-          <h4>{t("Quality Assurance (QA)")}</h4>
-          <Image src="/images/assurance.svg" width={218} height={218} className={Styles.Innovation1} alt='Innovation1' />
+          <div onMouseEnter={handleMouseEnter7} onMouseLeave={handleMouseLeave7}>
+            {!isMouseOver7 ? (
+              <>
+                <h4>{t("Quality Assurance (QA)")}</h4>
+                <Image src="/images/assurance.svg" width={218} height={218} className={Styles.Innovation1} alt='Innovation1' />
+              </>
+            ) : (
+              <>
+                <h4>{t("Quality Assurance (QA)")}</h4>
+                <p>{t("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}</p>
+                <Link href={'/contact'} className='c-button fillBtn'>{t("Inquire Now")}</Link>
+              </>
+            )}
+          </div>
         </Col>
       </Row>
 
